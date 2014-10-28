176ca567a8237062ecdacababaab5432b0616a89
andrea <battaglia.andry@gmail.com>
2014-10-28 19:30:54 +0100

    change cart position

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,4 +1,10 @@
 class StoreController < ApplicationController
+
+  #METODO PER TROVARE IL CARRELLO RELATIVO ALLA SESSIONE CORRENTE
+  #INCLUDEREMO PROPRIO IL CURRENT CART PER TROVARE IL CARRELLO NELLA SESSIONE
+  include CurrentCart
+  before_action :set_cart
+
   def index
     #STO CHIEDENDO UNA LISTA DEI CONTROLLER ORDINATA PER TITOLO
     @products = Product.order(:title)
