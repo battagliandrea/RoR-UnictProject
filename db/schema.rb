f7c607245eaf2a053d8b84ffbfd5d2fb5998c76e
andrea <battaglia.andry@gmail.com>
2014-10-29 18:10:06 +0100

    add user confirmable mail

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141029154041) do
+ActiveRecord::Schema.define(version: 20141029164730) do
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
@@ -76,6 +76,7 @@ ActiveRecord::Schema.define(version: 20141029154041) do
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
