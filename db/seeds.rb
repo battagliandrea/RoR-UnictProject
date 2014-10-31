01d8c01aa5d69d987184109ef9789263d09e4116
andrea <battaglia.andry@gmail.com>
2014-10-31 19:51:56 +0100

    add three elements into seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,38 +8,38 @@
 #---
 # encoding: utf-8
 Product.delete_all
-Product.create!(title: 'CoffeeScript',
+Product.create!(title: 'Maglia da calcio Juventus FC 2014/15(Casa)',
                 description:
-                    %{<p>
-        CoffeeScript is JavaScript done right. It provides all of JavaScript's
-	functionality wrapped in a cleaner, more succinct syntax. In the first
-	book on this exciting new language, CoffeeScript guru Trevor Burnham
-	shows you how to hold onto all the power and flexibility of JavaScript
-	while writing clearer, cleaner, and safer code.
-      </p>},
-                image_url:   'cs.jpg',
-                price: 36.00)
+                    %{
+        La maglia da calcio Juventus FC Stadium 2014/15 -
+        Uomo è realizzata in tessuto traspirante per una leggerezza e un comfort imbattibili.
+        Con stemma della squadra in tessuto e dettagli grafici in stile replica,
+        questa maglia replica della prima divisa rende omaggio alla tua squadra del cuore.
+      },
+                image_url:   'http://www.soccerfollowers.net/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/p/l/player_version_juventus_home_jersey_14-15_1_2.jpg',
+                price: 121.00)
 # . . .
-Product.create!(title: 'Programming Ruby 1.9 & 2.0',
+Product.create!(title: 'Maglia da calcio Juventus FC 2014/15(Trasferta)',
                 description:
-                    %{<p>
-        Ruby is the fastest growing and most exciting dynamic language
-        out there. If you need to get working programs delivered fast,
-        you should add Ruby to your toolbox.
-      </p>},
-                image_url: 'ruby.jpg',
-                price: 49.95)
+                    %{
+        La maglia da calcio Juventus FC Stadium 2014/15 -
+        Uomo è realizzata in tessuto traspirante per una leggerezza e un comfort imbattibili.
+        Con stemma della squadra in tessuto e dettagli grafici in stile replica,
+        questa maglia replica della seconda divisa rende omaggio alla tua squadra del cuore.
+      },
+                image_url: 'http://www.soccerfollowers.net/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/p/l/player_version_juventus_away_jersey_2014-15_1.jpg',
+                price: 86.00)
 # . . .
 
-Product.create!(title: 'Rails Test Prescriptions',
+Product.create!(title: 'Maglia da calcio Real Madred 2014/15',
                 description:
-                    %{<p>
-        <em>Rails Test Prescriptions</em> is a comprehensive guide to testing
-        Rails applications, covering Test-Driven Development from both a
-        theoretical perspective (why to test) and from a practical perspective
-        (how to test effectively). It covers the core Rails testing tools and
-        procedures for Rails 2 and Rails 3, and introduces popular add-ons,
-        including Cucumber, Shoulda, Machinist, Mocha, and Rcov.
-      </p>},
-                image_url: 'rtp.jpg',
-                price: 34.95)
+                    %{
+        La maglia bianca casalinga indossata dai Blancos al Bernabeu è una divisa leggendaria.
+        Arricchita da dettagli rosa che le conferiscono un look giovane,
+        questa maglia Home Replica Player Real Madrid presenta tutti i segni distintivi
+        di una delle migliori squadre al mondo,
+        fra cui la tecnologia di ventilazione climacool® per mantenere il corpo fresco e asciutto,
+        lo stemma della squadra sul cuore e lo stemma della LFP sulla manica destra.
+      },
+                image_url: 'http://www.soccerfollowers.net/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/r/e/real_madrid_home_soccer_jersey_2014-15__1.jpg',
+                price: 90.00)
