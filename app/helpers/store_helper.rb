00ab1e9c46616753419de81f23d0134db3f11b13
andrea <battaglia.andry@gmail.com>
2014-10-31 11:14:07 +0100

    store view with fixed cart

--- app/helpers/store_helper.rb
+++ app/helpers/store_helper.rb
@@ -1,11 +1,3 @@
 module StoreHelper
-#PRENDE UNA CONDIZIONE UN SET DI ATTRIBUTI E UN BLOCCO
-#E RACCHIUDE L'OUTPUT DENTRO UN div
-  def hidden_div_if(condition, attributes = {}, &block)
-    if condition
-      attributes["style"] = "display: none"
-    end
-    #con &block PASSO A content_tag IL BLOCCO FORNITO AL METODO
-    content_tag("div", attributes, &block)
-    end
+
 end
